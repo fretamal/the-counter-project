c81cc07ee5e61513e5b9610deadde1d4778fb5bf
Fernanda Retamal <fernanda.retamal@ideauno.cl>
2020-02-26 23:45:50 -0300

    test unitarios de un contador

---
+++ src/components/counter.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render, fireEvent, queryByText, queryByAltText} from '@testing-library/react'
+import  Counter  from './counter'
+
+it("renderiza correctamente el componente", () => {
+    const {queryByText,queryByAltText} = render(<Counter />)
+    expect(queryByText('+')).toBeTruthy()
+    expect(queryByText('-')).toBeTruthy()
+    expect(queryByAltText('Borrar')).toBeTruthy()
+})
+
+describe('funcionalidad del contador', () => {
+    it("Que se llame a incrementCount al apretar +", () => {
+        const props = {incrementCount: jest.fn()}
+        const {queryByText} = render(<Counter {...props}/>)
+        const buttonPlus =  queryByText('+')
+        fireEvent.click(buttonPlus)
+        expect(props.incrementCount).toHaveBeenCalled()
+    })
+
+    it("Que se llame a decrementCount al apretar -", () => {
+        const props = {decrementCount: jest.fn()}
+        const {queryByText} = render(<Counter {...props}/>)
+        const buttonMinus =  queryByText('-')
+        fireEvent.click(buttonMinus)
+        expect(props.decrementCount).toHaveBeenCalled()
+    })
+
+    it("Que se llame a deleteCounter al apretar borrar", () => {
+        const props = {deleteCounter: jest.fn()}
+        const {queryByAltText} = render(<Counter {...props}/>)
+        const buttonDelete =  queryByAltText('Borrar')
+        fireEvent.click(buttonDelete)
+        expect(props.deleteCounter).toHaveBeenCalled()
+    })
+})
